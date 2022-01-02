7be0cadfc57dea41a62c5cddfced697986ea9ffa
MD_IMRAN_HOSSAIN <imranmadbar@gmail.com>
2022-01-02 16:12:11 +0600

    add project oauth2 login with github

---
+++ spring-oauth2-github-login/src/main/java/com/imranmadbar/SpringBootOauth2GithubLoginApp.java
@@ -0,0 +1,13 @@
+package com.imranmadbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2GithubLoginApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2GithubLoginApp.class, args);
+	}
+
+}
